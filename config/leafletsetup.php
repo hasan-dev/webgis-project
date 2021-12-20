3cef42c445e288a5f9430bb35ffc47b25dc31cbc
hasan-dev <hasan01nov@gmail.com>
2021-12-20 16:01:15 +0700

    First Commit

---
+++ config/leafletsetup.php
@@ -0,0 +1,7 @@
+<?php
+return [
+    'zoom_level'           => 13,
+    'detail_zoom_level'    => 20,
+    'map_center_latitude'  => env('MAP_CENTER_LATITUDE', '-7.870949'),
+    'map_center_longitude' => env('MAP_CENTER_LONGITUDE', '111.462229'),
+];
